039f9f89faa39739c9ed9ec6494b43300a2fd75b
Iskandar-Karimov <iskandarworkofficial@gmail.com>
2025-01-02 19:36:55 +0500

    Go Fish Game

--- Card.py
+++ Card.py
@@ -5,13 +5,14 @@ class Card():
         self.suit = suit
         self.rank = rank
     def __str__(self):
-        return f"Card: {self.suit}, {self.rank}"
+        return f"{self.suit}, {self.rank}"
     
 class Deck():
     def __init__(self):
+        self.cards=[]
+
         suits = ["Spades", "Hearts", "Diamonds", "Clubs"]
-        ranks = [1,2,3]
-        self.cards = []
+        ranks = [1,2,3,4,5,6,7,8,9,10]
         for i in range(len(suits)):
             for j in range(len(ranks)):
                 self.cards.append(Card(suit=suits[i], rank=ranks[j]))
@@ -23,3 +24,15 @@ class Deck():
     def print(self):
         for card in self.cards:
             print(str(card))
+    
+    def get_cards(self, num_cards = 1):
+        given_cards = []
+        for i in range(num_cards):
+            given_cards.append(self.cards.pop(0))
+        return given_cards
+        
+
+
+
+
+    
---
+++ Go_Fish.py
@@ -0,0 +1,92 @@
+from Card import Card
+from Card import Deck
+
+import random
+
+class Player():
+    def __init__(self, cards):
+        self.cards = cards
+        self.score = 0
+
+    def print(self):
+        for card in self.cards:
+            print(str(card))
+
+    def check_cards(self, rank):
+        found_cards = []
+        index = 0
+        while index < len(self.cards):
+            if self.cards[index].rank == rank:
+                found_cards.append(self.cards.pop(index))
+            else:
+                index += 1
+        return found_cards
+    
+    def add_cards(self, found_cards):
+        self.cards = self.cards + found_cards
+
+    def check_for_four_cards(self):
+        for i in range(1, 11):
+            count = sum(card.rank == i for card in self.cards)
+            if count == 4:
+                self.cards = [card for card in self.cards if card.rank != i]
+                self.score += 1
+    
+    
+
+# deck = Deck()
+# deck.shuffle()
+# myPlayer = Player(cards = deck.get_cards(5))
+# myPlayer2 = Player(cards= deck.get_cards(5))
+# print("Player 1:")
+# myPlayer.print()
+# print("\nPlayer 2")
+# myPlayer2.print()
+# returnedCards = myPlayer.check_cards(8)
+# print("Returned card from Player 1:")
+# for card in returnedCards:
+#     print(str(card))
+# print()
+# print("Player 1's new cards:")
+# myPlayer.print()
+
+# print("\nPlayer 2's new cards:")
+# myPlayer2.add_cards(returnedCards)
+# myPlayer2.print()
+
+#Initializing
+deck = Deck()
+deck.shuffle()
+Human = Player(cards=deck.get_cards(5))
+Computer = Player(cards=deck.get_cards(5))
+
+#Human's turn, guessing from computer
+while len(Human.cards) > 0 and len(Computer.cards) > 0:
+    print("Here are your cards:")
+    Human.print()
+    requested_card = int(input("\nWhat card do you want to request?: "))
+    found = Computer.check_cards(requested_card)
+    print("The opponent had these cards:")
+    go = input("")
+    if found == []:
+        print("\nGO FISH!\n")
+        Human.add_cards(deck.get_cards())
+    for card in found:
+        print(card)
+    Human.add_cards(found_cards=found)
+    print(f"\nNew Cards: ")
+    Human.print()
+    Human.check_for_four_cards()
+    print("")
+
+    #Computer's turn, guessing from human
+    computerturn = input("Computer's Turn (Enter to continue): ")
+    requested_card2 = Computer.cards[0]
+    found2 = Human.check_cards(requested_card2.rank)
+    print(f"The computer guessed this card: {requested_card2}")
+    if found2 == []:
+        print("\nGO FISH!\n")
+        Computer.add_cards(deck.get_cards())
+    Computer.add_cards(found_cards=found2)
+    Computer.check_for_four_cards()
+    print(" ")
